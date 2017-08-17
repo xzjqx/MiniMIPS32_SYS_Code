0d828cfd6c10edf20b38c2c5777569999c6bf67f
xzjqx <xzjqx@outlook.com>
2017-08-17 17:10:28 +0800

    命名规范_处理器端口信号

--- sources_1/new/MiniMIPS32.v
+++ sources_1/new/MiniMIPS32.v
@@ -23,28 +23,28 @@
 `include "defines.v"
 
 module MiniMIPS32(
-	input wire clk,
-	input wire rst,
+	input wire cpu_clk_75M,
+	input wire cpu_rst_n,
 
 	//instruction wishbone interface signal
-	input wire[31:0] iwishbone_data_i,
-	input wire iwishbone_ack_i,
-	output wire[31:0] iwishbone_addr_o,
-	output wire[31:0] iwishbone_data_o,
-	output wire iwishbone_we_o,
-	output wire[3:0] iwishbone_sel_o,
-	output wire iwishbone_stb_o,
-	output wire iwishbone_cyc_o,
+	input wire[31:0] imem_inst_i,
+	input wire imem_ack_i,
+	output wire[31:0] imem_addr_o,
+	output wire[31:0] imem_inst_o,
+	output wire imem_we_o,
+	output wire[3:0] imem_sel_o,
+	output wire imem_stb_o,
+	output wire imem_cyc_o,
 	
 	//data wishbone interface signal
-	input wire[31:0] dwishbone_data_i,
-	input wire dwishbone_ack_i,
-	output wire[31:0] dwishbone_addr_o,
-	output wire[31:0] dwishbone_data_o,
-	output wire dwishbone_we_o,
-	output wire[3:0] dwishbone_sel_o,
-	output wire dwishbone_stb_o,
-	output wire dwishbone_cyc_o,
+	input wire[31:0] dmem_data_i,
+	input wire dmem_ack_i,
+	output wire[31:0] dmem_addr_o,
+	output wire[31:0] dmem_data_o,
+	output wire dmem_we_o,
+	output wire[3:0] dmem_sel_o,
+	output wire dmem_stb_o,
+	output wire dmem_cyc_o,
 	
 	input wire[2:0] s0_msel,
 	
@@ -257,7 +257,7 @@ module MiniMIPS32(
 	wire		rom_ce;
 
 	// pc_reg例化
-	PC pc0(.clk(clk), .rst(rst), .pc(pc),
+	PC pc0(.clk(cpu_clk_75M), .rst(cpu_rst_n), .pc(pc),
 			 .branch_flag_i(pc_branch_flag_i), .branch_target_address_i(pc_branch_target_address_i),
 			 .stall(stall),
 			 .cp0_branch_flag(cp0_exc_jump_flag),
@@ -267,7 +267,7 @@ module MiniMIPS32(
 
 	IF if0 (
 		.addr_i(pc), 
-		.rst(rst),
+		.rst(cpu_rst_n),
 		.inst_addr_o(inst_addr), 
 		.addr_o(if_addr_o), 
 		.exc_code_o(if_exc_code_o),
@@ -277,8 +277,8 @@ module MiniMIPS32(
     );
 	
 	iwishbone_bus_if iwishbone_bus_if(
-    	.clk(clk),
-    	.rst(rst),
+    	.clk(cpu_clk_75M),
+    	.rst(cpu_rst_n),
     	
     	.s0_msel(s0_msel),
     
@@ -292,20 +292,20 @@ module MiniMIPS32(
     	.cpu_sel_i(4'b1111),
     	.cpu_data_o(if_inst_o),
 						
-    	.wishbone_data_i(iwishbone_data_i),
-    	.wishbone_ack_i(iwishbone_ack_i),
-    	.wishbone_addr_o(iwishbone_addr_o),
-    	.wishbone_data_o(iwishbone_data_o),
-    	.wishbone_we_o(iwishbone_we_o),
-    	.wishbone_sel_o(iwishbone_sel_o),
-    	.wishbone_stb_o(iwishbone_stb_o),
-    	.wishbone_cyc_o(iwishbone_cyc_o),
+    	.wishbone_data_i(imem_inst_i),
+    	.wishbone_ack_i(imem_ack_i),
+    	.wishbone_addr_o(imem_addr_o),
+    	.wishbone_data_o(imem_inst_o),
+    	.wishbone_we_o(imem_we_o),
+    	.wishbone_sel_o(imem_sel_o),
+    	.wishbone_stb_o(imem_stb_o),
+    	.wishbone_cyc_o(imem_cyc_o),
 						
     	.stallreq(stop_from_if)    
 	);
 	
 	// IF/ID模块例化
-	IF_ID if_id0(.clk(clk), .rst(rst),
+	IF_ID if_id0(.clk(cpu_clk_75M), .rst(cpu_rst_n),
 				.if_pc(if_addr_o),
 				.if_inst(if_inst_o),
 				.exc_code_i(if_exc_code_o),
@@ -318,7 +318,7 @@ module MiniMIPS32(
 				.exc_badvaddr_o(id_exc_badvaddr_i));
 
 	// 译码阶段ID模块例化
-	ID id0(.rst(rst), .pc_i(id_pc_i), .pc_o(id_pc_o), .inst_i(id_inst_i),
+	ID id0(.rst(cpu_rst_n), .pc_i(id_pc_i), .pc_o(id_pc_o), .inst_i(id_inst_i),
 			 .reg1_data_i(reg1_data), .reg2_data_i(reg2_data),
 			 .ex_wreg(ex_wreg_o), .ex_wdata(ex_wdata_o), .ex_wd(ex_wd_o),
 			 .mem_wreg(mem_wreg_o), .mem_wdata(mem_wdata_o), .mem_wd(mem_wd_o),
@@ -340,12 +340,12 @@ module MiniMIPS32(
 			 .exc_badvaddr_o(id_exc_badvaddr_o));
 	
 	// 通用寄存器Regfile模块例化
-	REG reg0(.clk(clk), .rst(rst), .we(wb_wreg_i), .waddr(wb_wd_i), .wdata(wb_wdata_i),
+	REG reg0(.clk(cpu_clk_75M), .rst(cpu_rst_n), .we(wb_wreg_i), .waddr(wb_wd_i), .wdata(wb_wdata_i),
 				.re1(reg1_read), .raddr1(reg1_addr), .rdata1(reg1_data),
 				.re2(reg2_read), .raddr2(reg2_addr), .rdata2(reg2_data));
 	
 	// ID/EX模块例化
-	ID_EX id_ex0(.clk(clk), .rst(rst), .id_alusel(id_alusel_o), .id_aluop(id_aluop_o),
+	ID_EX id_ex0(.clk(cpu_clk_75M), .rst(cpu_rst_n), .id_alusel(id_alusel_o), .id_aluop(id_aluop_o),
 					 .id_reg1(id_reg1_o), .id_reg2(id_reg2_o), .id_wd(id_wd_o), .id_wreg(id_wreg_o),
 					 .id_is_in_delayslot(id_in_delay_o), .id_link_address(id_link_addr_o), .next_inst_in_delayslot_i(id_next_delay),
 					 .id_inst(id_inst_o), .id_pc(id_pc_o),
@@ -363,7 +363,7 @@ module MiniMIPS32(
 					 .exc_badvaddr_o(ex_exc_badvaddr_i));
 	
 	// EX模块例化
-	EX ex0(.rst(rst), .alusel_i(ex_alusel_i), .aluop_i(ex_aluop_i), .pc_i(ex_pc_i), .pc_o(ex_pc_o),
+	EX ex0(.rst(cpu_rst_n), .alusel_i(ex_alusel_i), .aluop_i(ex_aluop_i), .pc_i(ex_pc_i), .pc_o(ex_pc_o),
 			 .reg1_i(ex_reg1_i), .reg2_i(ex_reg2_i),
 			 .wd_i(ex_wd_i), .wreg_i(ex_wreg_i),
 			 .hi_i(ex_hi_i), .lo_i(ex_lo_i), 
@@ -398,7 +398,7 @@ module MiniMIPS32(
              .signed_div_o(signed_div),    
 			 .stop(stop_from_ex));
 	
-	EX_MEM ex_mem0(.clk(clk), .rst(rst), .ex_wd(ex_wd_o), .ex_wreg(ex_wreg_o), .ex_wdata(ex_wdata_o),
+	EX_MEM ex_mem0(.clk(cpu_clk_75M), .rst(cpu_rst_n), .ex_wd(ex_wd_o), .ex_wreg(ex_wreg_o), .ex_wdata(ex_wdata_o),
 						.ex_whilo(ex_whilo_o), .ex_hi(ex_hi_o), .ex_lo(ex_lo_o),
 						.ex_aluop(ex_aluop_o), .ex_mem_addr(ex_mem_addr_o), .ex_reg2(ex_reg2_o),
 						.mem_wd(mem_wd_i), .mem_wreg(mem_wreg_i),	.mem_wdata(mem_wdata_i),
@@ -422,7 +422,7 @@ module MiniMIPS32(
 					   .ex_pc(ex_pc_o), .mem_pc(mem_pc_i));
 	
 	// MEM模块例化 
-	MEM mem0(.rst(rst), .wd_i(mem_wd_i), .wreg_i(mem_wreg_i), .wdata_i(mem_wdata_i), .in_delay_i(mem_in_delay_i), .pc_i(mem_pc_i),
+	MEM mem0(.rst(cpu_rst_n), .wd_i(mem_wd_i), .wreg_i(mem_wreg_i), .wdata_i(mem_wdata_i), .in_delay_i(mem_in_delay_i), .pc_i(mem_pc_i),
 				.whilo_i(mem_whilo_i), .hi_i(mem_hi_i), .lo_i(mem_lo_i),
 				.aluop_i(mem_aluop_i), .mem_addr_i(mem_addr_i), .reg2_i(mem_reg2_i), .mem_data_i(mem_data_i),
 				.wd_o(mem_wd_o), .wreg_o(mem_wreg_o), .wdata_o(mem_wdata_o),
@@ -444,8 +444,8 @@ module MiniMIPS32(
 				.in_delay_o(cp0_in_delay_i));
 	
 	dwishbone_bus_if dwishbone_bus_if(
-    	.clk(clk),
-    	.rst(rst),
+    	.clk(cpu_clk_75M),
+    	.rst(cpu_rst_n),
     	
     	.s0_msel(s0_msel),
     
@@ -459,20 +459,20 @@ module MiniMIPS32(
     	.cpu_sel_i(mem_sel_o),
     	.cpu_data_o(mem_data_i),
 						
-    	.wishbone_data_i(dwishbone_data_i),
-    	.wishbone_ack_i(dwishbone_ack_i),
-    	.wishbone_addr_o(dwishbone_addr_o),
-    	.wishbone_data_o(dwishbone_data_o),
-    	.wishbone_we_o(dwishbone_we_o),
-    	.wishbone_sel_o(dwishbone_sel_o),
-    	.wishbone_stb_o(dwishbone_stb_o),
-    	.wishbone_cyc_o(dwishbone_cyc_o),
+    	.wishbone_data_i(dmem_data_i),
+    	.wishbone_ack_i(dmem_ack_i),
+    	.wishbone_addr_o(dmem_addr_o),
+    	.wishbone_data_o(dmem_data_o),
+    	.wishbone_we_o(dmem_we_o),
+    	.wishbone_sel_o(dmem_sel_o),
+    	.wishbone_stb_o(dmem_stb_o),
+    	.wishbone_cyc_o(dmem_cyc_o),
 						
     	.stallreq(stop_from_mem)       
 	);
 	
 	// MEM/WB模块例化			
-	MEM_WB mem_wb0(.clk(clk), .rst(rst),
+	MEM_WB mem_wb0(.clk(cpu_clk_75M), .rst(cpu_rst_n),
 						.mem_wd(mem_wd_o), .mem_wreg(mem_wreg_o),	.mem_wdata(mem_wdata_o),
 						.mem_whilo(mem_whilo_o), .mem_hi(mem_hi_o), .mem_lo(mem_lo_o),
 						.wb_wd(wb_wd_i), .wb_wreg(wb_wreg_i), .wb_wdata(wb_wdata_i),
@@ -480,12 +480,12 @@ module MiniMIPS32(
 						.stall(stall),
 						.flush(flush));
 						
-	HILO hilo0(.clk(clk), .rst(rst), .we(wb_whilo_i),
+	HILO hilo0(.clk(cpu_clk_75M), .rst(cpu_rst_n), .we(wb_whilo_i),
 				  .hi_i(wb_hi_i), .lo_i(wb_lo_i), .hi_o(ex_hi_i), .lo_o(ex_lo_i));
 	
 	CP0 cp0 (
-		.clk(clk), 
-		.rst(rst), 
+		.clk(cpu_clk_75M), 
+		.rst(cpu_rst_n), 
 		.we_i(ex_mem_cp0_reg_we), 
 		.waddr_i(ex_mem_cp0_write_addr), 
 		.raddr_i(ex_cp0_reg_read_addr_o), 
@@ -510,8 +510,8 @@ module MiniMIPS32(
 	);
 	
 	CTRL ctrl0 (
-    .clk(clk), 
-    .rst(rst), 
+    .clk(cpu_clk_75M), 
+    .rst(cpu_rst_n), 
     .stop_from_id(stop_from_id), 
     .stop_from_ex(stop_from_ex), 
     .stop_from_mem(stop_from_mem), 
@@ -523,8 +523,8 @@ module MiniMIPS32(
     );
     
     DIV div0(
-        .clk(clk),
-        .rst(rst),
+        .clk(cpu_clk_75M),
+        .rst(cpu_rst_n),
     
         .signed_div_i(signed_div),
         .opdata1_i(div_opdata1),
